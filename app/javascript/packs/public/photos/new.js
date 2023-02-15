1fdf25e869df337fc9315db64e6e52c7bcc3ce24
EC2 Default User <ec2-user@ip-172-31-0-152.ap-northeast-1.compute.internal>
2023-02-15 10:25:07 +0000

    [update layout]Create_photo_arts

--- app/javascript/packs/public/photos/new.js
+++ app/javascript/packs/public/photos/new.js
@@ -6,7 +6,7 @@ $(function () {
   $('.js-searchable-multiple').select2({
     dropdownAutoWidth: true,
     theme: 'bootstrap4',
-    placeholder: "都道府県検索",
+    placeholder: "タグ選択",
     allowClear: true,
     multiple: true
   });
