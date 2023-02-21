25a87bf227a9010878a086d85839473b0a360c41
EC2 Default User <yj.r122448@gmail.com>
2023-02-21 10:12:20 +0000

    [update layout]Create_photo_arts

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,7 +5,7 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.photo_id = @photo.id
     if @comment.save
-      redirect_to @photo
+      render :comments
     else
       @photo_comment = Photo.new
       render "public/photos/show"
@@ -14,7 +14,8 @@ class Public::CommentsController < ApplicationController
 
   def destroy
     Comment.find_by(id: params[:id], photo_id: params[:photo_id]).destroy
-    redirect_to request.referer
+    @photo = Photo.find(params[:photo_id])
+    render :comments
   end
 
 
